d81c31ff4213e06174f6eba0032db4906e33785e
Stephen Hulme <sh51@sanger.ac.uk>
2025-09-16 11:41:20 +0100

    Add command line unit tests

---
+++ tests/test_hts_synth.py
@@ -0,0 +1,12 @@
+from click.testing import CliRunner
+from hts_synth.hts_synth import cli
+import random
+
+
+def test_hello_world(monkeypatch):
+    random.seed(42)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--length=20"])
+    assert result.exit_code == 0
+    assert result.output == "GACAGGTACAAGAAGGAGTA\nPKriXrefSFPLBYtCRGSE\n"
