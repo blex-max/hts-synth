251c4c73ba29574f33ce3c4fbd255c3ee86acfba
blex-max <ablex7@gmail.com>
2025-09-17 12:25:20 +0100

    cli can now output fastq format

--- hts_synth/hts_synth.py
+++ hts_synth/hts_synth.py
@@ -1,31 +1,54 @@
+from typing import Literal
 import click
+import uuid
 
 from .reads.read_generator import QualityModel, ReadGenerator
 from .ref.enums import VariantType
 
 
-@click.command()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.option(
-    "--reference_position",
-    default=100,
+    "-p",
+    "--reference-position",
+    default=0,
+    show_default=True,
+    type=int,
     help="The starting position within the reference genome where this read originates.",
 )
 @click.option(
-    "--reference_sequence",
-    default=None,
-    help="The reference DNA sequence to use as the basis for read generation.",
-)
-@click.option(
-    "--error_probabilities",
-    default=None,
+    "--error-probabilities",
+    default=None,  # TODO: surface the default
     help="""JSON string mapping variant type numbers to error probabilities, 
     e.g., \'{"0": 0.01, "1": 0.01, "2": 0.02}\' 
     (0=INSERTION, 1=DELETION, 2=SUBSTITUTION)""",
+)
+@click.option(
+    "-f",
+    "--out-format",
+    show_default=True,
+    default="fq",
+    type=click.Choice(["fq", "seq", "qual"]),
+    help="format of the output"
+)
+@click.argument(
+    "n-reads",
+    metavar='NREADS',
+    default=1,
+    type=int,
+)
+@click.argument(
+    "reference-sequence",
+    metavar="REF"
 )
 def cli(
     reference_position: int,
     reference_sequence: str,
     error_probabilities: dict[VariantType, float] | None = None,
+    n_reads: int = 1,
+    out_format: Literal["fq", "seq", "qual"] = "fq"  # should probably use an enum
 ):
     """
     Generate synthetic HTS read data from a reference sequence.
@@ -36,8 +59,8 @@ def cli(
     to simulate realistic sequencing errors.
 
     Args:
-        reference_position: The starting genomic position for the read (default: 100)
-        reference_sequence: The DNA reference sequence string (default: "ATGCTGTG")
+        reference_position: The starting genomic position for the read (default: 0)
+        reference_sequence: The DNA reference sequence string
         error_probabilities: Optional mapping of variant types to error rates
 
     Returns:
@@ -46,6 +69,15 @@ def cli(
     quality_model = QualityModel()
     generator = ReadGenerator(quality_model=quality_model, error_probabilities=error_probabilities)
 
-    read = generator.generate(reference_position, reference_sequence)
-    click.echo(read.query_sequence)
-    click.echo(read.query_qualities_str)
+    for _ in range(n_reads):
+        read = generator.generate(reference_position, reference_sequence)
+        match out_format:
+            case "fq":
+                click.echo(f"@read-{str(uuid.uuid4())[:16]}")
+                click.echo(read.query_sequence)
+                click.echo('+')
+                click.echo(read.query_qualities_str)
+            case "seq":
+                click.echo(read.query_sequence)
+            case "qual":
+                click.echo(read.query_qualities_str)
