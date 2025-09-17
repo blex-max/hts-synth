1955e741e6c7f75ae0dafb51312915d408b711cc
Stephen Hulme <sh51@sanger.ac.uk>
2025-09-17 09:39:10 +0100

    Lint

--- examples/run_variant_generation.py
+++ examples/run_variant_generation.py
@@ -5,4 +5,3 @@ generator = VariantGenerator(sequence=("ACTTGGAAGT"), events=[1, 1, 1])
 variants = generator.generate_random_variant_sequence()
 alt_length = len(variants)
 print(apply_variants(ref_start=0, ref_seq="ACTTGGAAGT", alt_length=alt_length, variants=variants))
-
