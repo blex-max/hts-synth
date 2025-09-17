2a8814d7b9d505e1bcdd60fed31a47dcc5454cda
Willem van Heerden <wh5@sanger.ac.uk>
2025-09-17 10:02:01 +0100

    Update unit test workflow, dev dependencies, and remove unused utility function
    - Added system dependency installation step in workflow for Pysam support.
    - Updated `requirements-dev.txt` to fix duplicate `matplotlib` entry.
    - Removed unused `safe_group_by` function from `utils.py`.

--- hts_synth/ref/utils.py
+++ hts_synth/ref/utils.py
@@ -22,10 +22,6 @@ def parse_opt_int_group(m: re.Match[str], i: int) -> int:
     return int(g) if g else 0
 
 
-def safe_group_by(a: Iterable[Any], k: Callable[[Any], int]):
-    return groupby(sorted(a, key=k), key=k)
-
-
 def has_duplicates(items: list[str]) -> bool:
     return len(set(items)) != len(items)
 
