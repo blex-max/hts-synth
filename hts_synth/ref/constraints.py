152cd8bffc942eb215dd008a8a4778337098ee2b
Hitham Hassan <hh12@sanger.ac.uk>
2025-09-17 12:20:02 +0100

    Add basic constraints class with a placeholder for model

--- hts_synth/ref/constraints.py
+++ hts_synth/ref/constraints.py
@@ -3,6 +3,7 @@ from collections.abc import Sequence
 
 from .enums import VariantType
 from .variant import Variant
+from ..model.qual_model import NaiveQualModelBase
 
 
 class EditEvent:
@@ -10,7 +11,6 @@ class EditEvent:
     Represents a single edit event to be applied to a read.
     """
     ALLOWED_VALS_ON_READ = ["r1", "r2", "both"]
-
 
     def __init__(
         self,
@@ -28,3 +28,18 @@ class EditEvent:
             raise ValueError(message)
 
         self.on_read = on_read
+
+class Constraints:
+    """
+    Imposes post-generation edits on sequencing reads, placeholder for
+    model (not yet implemented).
+    """
+    def __init__(self, *edit_events: EditEvent, model: NaiveQualModelBase=None):
+        self.edit_events = list(edit_events)
+
+    def apply(self, read: str, read_name: str) -> str:
+        """
+        Applies all relevant edit events to the given read.
+        """
+        return read
+
